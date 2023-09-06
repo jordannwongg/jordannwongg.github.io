12cefa01b7b4185c8574c214d4895171a9c65acc
jordanwong2002 <jordanmwong2002@gmail.com>
2023-09-06 15:29:20 +0800

    trying again

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    output: 'export',
-    appDir: true,
-  }
+  output: "export"
 }
 
 module.exports = nextConfig
