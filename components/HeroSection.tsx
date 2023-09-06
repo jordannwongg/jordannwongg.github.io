fd83680aecb3cd56ba6d11c3a749f53bcc27117c
jordanwong2002 <jordanmwong2002@gmail.com>
2023-09-06 14:23:04 +0800

    new typer and packages

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,23 +3,9 @@ import React, { useState, useEffect, useMemo } from "react";
 import Image from "next/image"
 import { Link } from "react-scroll/modules"
 import { HiArrowDown } from "react-icons/hi"
-import useTypewriter from 'react-typewriter-hook'
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
-  const phrases = useMemo(() => ["Computer Science Student", "Software Engineer"], []);
-  const [index, setIndex] = useState(0);
-  const text = useTypewriter(phrases[index % phrases.length]);
-
-  useEffect(() => {
-    if (text === phrases[index % phrases.length]) { // When the current phrase is fully typed out
-      const timeout = setTimeout(() => { 
-        setIndex(prevIndex => prevIndex + 1);
-      }, 2000); // Wait 2 seconds before switching to the next phrase
-
-      return () => clearTimeout(timeout); // Cleanup the timeout if the component gets unmounted
-    }
-  }, [text, index, phrases]);
-
   return (
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-8 py-12 sm:py-24 md:py-32 md:flex-row md:space-x-4 md:text-left">
@@ -37,7 +23,14 @@ const HeroSection = () => {
           <span className="text-blue-500"> Jordan</span>!</h1>
           <p className="text-xl mt-2 mb-2 md:text-4xl fixed-height flex items-center">
             <span className="font-semibold text-gray-500">
-             {text}
+            <TypeAnimation 
+                cursor={true}
+                sequence={["Computer Science Student", "Software Engineer"]}
+                wrapper="b"
+                repeat={Infinity} 
+                speed={{type: 'keyStrokeDelayInMs', value: 110}}
+                deletionSpeed={{type: 'keyStrokeDelayInMs', value: 110}}
+              />
             </span>
           </p>
         </div>
