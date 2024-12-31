8ba54ddce85417d6db8c0cea182a630339ec39fd
Jordan Wong <141597382+jordannwongg@users.noreply.github.com>
2024-12-30 20:38:04 -0700

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
             src="/graduation.PNG"
             alt="headshot pic"
             width={325}
-            height={300}
+            height={200}
             className="rounded-full shadow-2xl"
           />
         </div>
